3ec5ba9c7f4ad3ed3e1a1281af83bc2e4ddea19d
jmcafferata <jmcafferata@gmail.com>
2017-08-13 15:59:00 -0300

    Waiter

---
+++ app/src/main/java/com/jmcafferata/menu/Item.java
@@ -0,0 +1,12 @@
+package com.jmcafferata.menu;
+
+/**
+ * Created by Franco on 13/08/2017.
+ */
+
+class Item {
+    public String nombre;
+    public int precio;
+    public String comentario;
+    public boolean estaListo;
+}
---
+++ app/src/main/java/com/jmcafferata/menu/Pedido.java
@@ -0,0 +1,14 @@
+package com.jmcafferata.menu;
+
+/**
+ * Created by Franco on 13/08/2017.
+ */
+
+public class Pedido {
+    public int total;
+    public int mesa;
+    public Item[] items;
+    public String horaLlegada;
+    public String horaPedido;
+    public String horaPartida;
+}
